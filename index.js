db8d98165b1496b0a9e5e8a077e04c0eaa24bcae
Harsh Ninave <harshninave58@gmail.com>
2024-06-03 18:37:11 +0530

    Project uploaded

---
+++ index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+// parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  fs.readdir(`./files`, (err, files) => {
+    res.render('index', { files: files });
+  });
+});
+
+// To read files and there's seperate .ejs files to view data
+app.get('/files/:filename', (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, 'utf-8', function (err, filedata) {
+    res.render('show', { filename: req.params.filename, filedata: filedata });
+  });
+});
+
+// To post content
+app.post('/create', (req, res) => {
+  fs.writeFile(`./files/${req.body.title}.txt`, req.body.details, (err) => {
+    if (err) throw err;
+    res.redirect('/');
+  });
+});
+
+app.listen('3000', () => {
+  console.log('Example app listening on port 3000!');
+});
